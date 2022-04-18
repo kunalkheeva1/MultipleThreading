dbee47a069f5c9fa02e23bd4c2cac28e4f08d7b7
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-17 20:52:41 -0700

    Initial commit

---
+++ src/MultipleThreading.java
@@ -0,0 +1,18 @@
+class Kunal extends Thread{
+    public void kk(){
+        System.out.println("Kheeva saab");
+
+    }
+
+}
+
+
+public class MultipleThreading {
+    public static void main(String[] args) {
+        Kunal nn = new Kunal();
+        for(int i=0; i<=6; i++){
+            System.out.println();
+        }
+    }
+
+}
